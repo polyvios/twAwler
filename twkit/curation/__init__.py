0cb98a065108878e567cfaac6a3a03fa8226ad58
polyvios <polyvios@ics.forth.gr>
2018-12-05 17:29:25 +0200

    init placeholder

---
+++ twkit/curation/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################
+# (c) 2016-2017 Polyvios Pratikakis
+# polyvios@ics.forth.gr
+###########################################
+
+#__all__ = ['utils']
+''' empty '''
