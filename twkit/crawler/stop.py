d430aefddcb1388254d1022fb945512f3e7a9c80
polyvios <polyvios@ics.forth.gr>
2018-12-05 16:55:59 +0200

    stop tracking a user and ignore them from now on

---
+++ twkit/crawler/stop.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################
+# (c) 2016-2018 Polyvios Pratikakis
+# polyvios@ics.forth.gr
+###########################################
+
+import sys
+import optparse
+from twkit.utils import *
+
+"""
+Remove the user from the tracked user set and add them to the ignored
+set.
+"""
+
+if __name__ == '__main__':
+  parser = optparse.OptionParser()
+  parser.add_option("--id", action="store_true", dest="ids", default=False, help="Input is ids, not usernames")
+  (options, args) = parser.parse_args()
+
+  db, api = init_state()
+  userlist = [x.lower().replace("@", "") for x in args]
+  for user in userlist:
+    uid = long(user) if options.ids else None
+    uname = None if options.ids else user
+    u = get_tracked(db, uid=uid, uname=uname)
+    if u:
+      db.following.delete_one(u)
+      db.greeks.delete_one({'id': u['id']})
+      db.frequences.delete_one({'id': u['id']})
+      ignore_user(db, u['id'])
+      print "stopped following, ignoring", u
+    else:
+      u = lookup_user(db, uid, uname)
+      print "user not tracked, ignoring anyway", uid, uname, u['screen_name']
+      ignore_user(db, u['id'])
