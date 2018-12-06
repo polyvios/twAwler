dcdb826360156e21c067695dd6cf8a326dee1306
polyvios <polyvios@ics.forth.gr>
2018-12-06 09:28:36 +0200

    remove duplicate user entries from the db, ignoring a set of fields (for subsampling)

---
+++ twkit/curation/userdedup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################
+# (c) 2016-2017 Polyvios Pratikakis
+# polyvios@ics.forth.gr
+###########################################
+
+import sys
+import re
+import time
+import optparse
+from progress.bar import Bar
+from datetime import datetime,timedelta
+from sets import Set
+from twkit.utils import *
+
+if __name__ == '__main__':
+  db, api = init_state()
+  cursor = db.users.find({'screen_name_lower': {'$gt': ''}})
+  for user in Bar("Users: ", max=cursor.count(), suffix = '%(index)d/%(max)d - %(eta_td)s').iter(cursor):
+    del user['_id']
+    if 'timestamp_at' in user: del user['timestamp_at']
+    if 'statuses_count' in user: del user['statuses_count']
+    if 'favourites_count' in user: del user['favourites_count']
+    cursor = db.users.find({'screen_name_lower': user['screen_name_lower']}).skip(1)
+    for user2 in cursor:
+      if user2['id'] != user['id']: print " user {} and {} have same screen name but different ids".format(user['id'], user2['id'])
+      db.users.update_one(user2, {'$unset': {'screen_name_lower':1}})
+  count = 0
+  cursor = db.users.find({'screen_name_lower': {'$gt': ''}})
+  for user in Bar("Users: ", max=cursor.count(), suffix = '%(index)d/%(max)d - %(eta_td)s').iter(cursor):
+    del user['_id']
+    if 'timestamp_at' in user: del user['timestamp_at']
+    if 'statuses_count' in user: del user['statuses_count']
+    if 'favourites_count' in user: del user['favourites_count']
+    user['screen_name_lower'] = None
+    cursor = db.users.find(user).skip(1)
+    del user['screen_name_lower']
+    for user2 in cursor:
+      inner_id = user2['_id']
+      del user2['_id']
+      if 'timestamp_at' in user2: del user2['timestamp_at']
+      if 'statuses_count' in user2: del user2['statuses_count']
+      if 'favourites_count' in user2: del user2['favourites_count']
+      if user == user2:
+        print " Delete {} {}".format(user2['screen_name'], user2['id'])
+        db.users.delete_one({'_id' : inner_id})
+        count += 1
