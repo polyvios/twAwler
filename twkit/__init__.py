4832056eb65fbce5769cda7e6e8d564d11e2ad5f
polyvios <polyvios@ics.forth.gr>
2018-12-07 11:55:14 +0200

    package info

--- twkit/__init__.py
+++ twkit/__init__.py
@@ -5,5 +5,27 @@
 # polyvios@ics.forth.gr
 ###########################################
 
-#__all__ = ['utils']
+"""
+A library for crawling twitter and analyzing crawled tweets and
+relations.  
+
+Currently extracts 6 kinds of relations:
+  * follow: unweighted, directed graph
+  * favorite: weighted, directed graph
+  * reply: weighted, directed graph
+  * retweet: weighted, directed graph
+  * quote: weighted, directed graph
+  * listsim: weighted, undirected graph
+
+Currently extracts around 2000 features per user.
+"""
+
+__author__       = 'Polyvios Pratikakis'
+__email__        = 'polyviosr@ics.forth.gr'
+__copyright__    = 'Copyright (c) 2016-present Polyvios Pratikakis, FORTH-ICS'
+__license__      = 'Apache License 2.0'
+__version__      = '0.0.1'
+__url__          = 'https://github.com/polyvios/twAwler'
+__description__  = 'A Twitter API crawler and feature extraction library'
+
 from utils import init_state, verbose
