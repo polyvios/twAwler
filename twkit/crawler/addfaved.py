757be7a777050a5436005e35b1f1db96cdc687c3
Polyvios Pratikakis <polyvios@ics.forth.gr>
2018-12-12 13:29:39 +0200

    support for --id

--- twkit/crawler/addfaved.py
+++ twkit/crawler/addfaved.py
@@ -15,16 +15,24 @@ import optparse
 from twkit.utils import *
 
 if __name__ == '__main__':
-  parser = optparse.OptionParser()
+  parser = optparse.OptionParser(usage=u'Usage: %prog [options] <user> [<user> ...]')
   parser.add_option("--refollow", action="store_true", dest="refollow", default=False, help="Re-follow ignored users")
+  parser.add_option("--id", action="store_true", dest="ids", default=False, help="Input is ids, not usernames")
+  parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Make noise")
   (options, args) = parser.parse_args()
-  if len(args) < 1:
+
+  if len(args) == 0:
     parser.print_help()
     sys.exit(1)
+
+  verbose(options.verbose)
   db, api = init_state()
   userlist = [x.lower().replace("@", "") for x in args]
-  for username in userlist:
-    for tw in api.GetFavorites(screen_name=username, count=200):
+
+  for user in userlist:
+    uid = long(user) if options.ids else None
+    username = None if options.ids else user
+    for tw in api.GetFavorites(user_id=uid, screen_name=username, count=200):
       j = pack_tweet(db, tw)
       try:
         db.tweets.insert_one(j)
