10f086fc4e963d6f92bc5998cf75b52189880f06
Polyvios Pratikakis <polyvios@ics.forth.gr>
2020-11-11 16:39:36 +0200

    py3

--- twkit/analytics/allusercreationdates.py
+++ twkit/analytics/allusercreationdates.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 ###########################################
-# (c) 2016-2018 Polyvios Pratikakis
+# (c) 2016-2020 Polyvios Pratikakis
 # polyvios@ics.forth.gr
 ###########################################
 
 """
-Computes a distribution of account creation times for all users, saved
+Computes a distribution of account creation times for given users, saved
 as CSV.
 """
 
@@ -17,10 +17,23 @@ from collections import defaultdict, Counter
 from twkit.utils import *
 
 if __name__ == '__main__':
+  parser = optparse.OptionParser(usage=u'Usage: %prog [options] <user> [<user> ...]')
+  parser.add_option("--id", action="store_true", dest="ids", default=False, help="Input is ids, not usernames")
+  parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Make noise")
+  (options, args) = parser.parse_args()
+
+  verbose(options.verbose)
   db, api = init_state(use_cache=False, ignore_api=True)
-  cursor = db.users.find({'screen_name_lower': {'$gt': ''}})
+
+  #todo: add an --all flag for this line
+  #cursor = db.users.find({'screen_name_lower': {'$gt': ''}})
+  cursor = (i.replace("@", "") for i in args)
+  if options.ids:
+    cursor = [long(i) for i in cursor]
+  maxc = len(cursor)
+  cursor = (lookup_user(db, i) for i in cursor)
   dates = defaultdict(lambda: Counter())
-  for user in Bar("Users: ", max=cursor.count(), suffix = '%(index)d/%(max)d - %(eta_td)s').iter(cursor):
+  for user in Bar("Users: ", max=maxc, suffix = '%(index)d/%(max)d - %(eta_td)s').iter(cursor):
     if 'created_at' not in user: continue
     d = user['created_at'].date()
     dates[d][user['id']] = 1
@@ -28,5 +41,5 @@ if __name__ == '__main__':
   maxdate = max(dates.keys())
   d = mindate
   while d < maxdate:
-    print d.isoformat(), len(dates[d])
+    print("{},{}".format(d.isoformat(), len(dates[d])))
     d += timedelta(days=1)
